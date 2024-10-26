8027243ad91147318bb18ce0ace903a88a1023b6
Giorgia Bazzanella <giorgiabazzanell@gmail.com>
2024-10-26 20:14:26 +0200

    esercizio finito

---
+++ 26-10-24/esercizio.js
@@ -0,0 +1,35 @@
+/* Primo esercizio 26-10-24 
+
+“Il valore della variabile num1 e' tot“ */
+
+
+let num1 = 59;
+let num2 = 14245;
+
+console.log(`Il valore della variabile è ${num1}.`);
+
+console.log(`Il valore della variabile è ${num2}.`);
+
+
+
+
+/* “Il valore della variabile stringa e' tot“*/
+
+let stringa = 'un testo'
+console.log(`Il valore della stringa è ${stringa}.`);
+
+/* sostituisci il valore della variabile stringa con un qualsiasi altro valore e poi stampalo a schermo.*/ 
+
+/* let stringa = 'non saprei di preciso'
+console.log(`Il valore della stringa è ${stringa}`); */
+
+
+/* “il valore di PIGRECO e' tot” */
+
+const PIGRECO = 3.14159265359;
+/* let PIGRECO= 4; */
+console.log(`Il valore di PIGRECO è ${PIGRECO}!!`);
+
+
+
+
